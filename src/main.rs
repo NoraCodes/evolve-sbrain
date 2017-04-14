e3ae06e256f41103eecbf66d61df151c17d55557
SilverWingedSeraph <lfstindall@gmail.com>
2017-04-14 10:44:54 -0500

    More mutation types and fix pop mutation

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,15 @@ use random::Source; // Trait for randomness iteration
 
 mod randomness;
 
-const SB_SYMBOLS: [char; 26] = ['<', '>', '-', '+', '.', ',', '[', ']', 
-                               '{', '}', '(', ')', 'z', '!', 's', 'S', 
-                               '@', '|', '&', '*', '^', 'a', 'd', 'q', 
-                               'm', 'p'];
+const SB_SYMBOLS: [char; 26] = ['<', '>', '-', '+', '.', ',', '[', ']',
+                                '{', '}', '(', ')', 'z', '!', 's', 'S', 
+                                '@', '|', '&', '*', '^', 'a', 'd', 'q', 
+                                'm', 'p'];
 
 const MUTATIONS_PER_CYCLE: usize = 3;
+const STARTING_LENGTH: usize = 32;
+const MAX_RUNTIME: u32 = 128;
+const POPULATION_SIZE: usize = 16;
 
 type Program = Vec<char>;
 type Population = Vec<(u64, Program)>;
@@ -33,37 +36,48 @@ fn generate_population(length: usize, individuals: usize) -> Population {
 fn mutate_program(mut program: Program) -> Program {
     let mut s = randomness::get_randomness();
     for _ in 0..MUTATIONS_PER_CYCLE {
-        let target_index: usize = s.read::<usize>() % program.len();
-        program[target_index] = SB_SYMBOLS[s.read::<usize>() % SB_SYMBOLS.len()];
+        let mutation_type = s.read::<usize>() % 3; // HACK! Make enum and match
+        if mutation_type == 0 {
+            let target_index: usize = s.read::<usize>() % program.len();
+            program[target_index] = SB_SYMBOLS[s.read::<usize>() % SB_SYMBOLS.len()];
+        } else if mutation_type == 1 {
+            program.push(SB_SYMBOLS[s.read::<usize>() % SB_SYMBOLS.len()]);
+        } else if mutation_type == 2 {
+            program.pop();
+        }
     }
     program
 }
 
 fn mutate_population(population: Population) -> Vec<Program> {
+    // Reserve one for the best and one for fresh blood
+    let empty_slots = population.len() - 2;
+    // Create buffer and iterator
+    let mut new_population = Vec::with_capacity(population.len());
+    let mut pop_iter = population.into_iter();
     // Preserve the best program, so no reverse progress happens
-    let best = population[0].1.clone();
-    // Length for the next-generated program.
-    let len = best.len();
+    new_population.push(pop_iter.next().unwrap().1);
+    // Mutate the best to fill half the new population
+    let best_program = new_population[0].clone();
+    for _ in 0..(empty_slots / 2) {
+        new_population.push(
+            mutate_program(
+                best_program.clone()
+            )
+        )
+    }
 
-    // Mutate all the programs except the best and worst
-    let mut mutated: Vec<Program> = population[1..population.len()-1]
-            .into_iter()
-            .map(move |p| { mutate_program(p.1.clone()) } )
-            .collect();
-    
+    // Now the best from the old population
+    pop_iter.take(empty_slots / 2).map(|prog| new_population.push(mutate_program(prog.1.clone()))).count();
 
-    let mut new: Vec<Program> = vec![best];
-    // Add in the mutated population
-    new.append(&mut mutated);
-    // Add one single totally new program
-    new.push(generate_random_program(len));
-
-    new
+    // Now fresh blood
+    new_population.push(generate_random_program(STARTING_LENGTH));
+    new_population
 }
 
 fn cost_program(program: &Program) -> u64 {
     let actual_output = "Hello, world!".to_string();
-    let res = sbrain::fixed_evaluate(&(program.iter().collect::<String>()), Some(vec![1,2,3,4,5]), Some(100));
+    let res = sbrain::fixed_evaluate(&(program.iter().collect::<String>()), Some(vec![0]), Some(100));
     let mut score = i64::abs(actual_output.len() as i64 - res.output.len() as i64) as u64 * 1024;
     for (expected, actual) in res.output.into_iter().zip(actual_output.chars()) {
         if expected != actual as u32 {
@@ -85,25 +99,29 @@ fn sort_population_by_cost(mut population: Population) -> Population {
 }
 
 fn main() {
-    let mut pop: Population = generate_population(128, 16);
+    let mut pop: Population = generate_population(STARTING_LENGTH, POPULATION_SIZE);
     let mut tries = 0;
     let mut last_cost = std::u64::MAX;
     loop {
         tries += 1;
         pop = cost_population(mutate_population(pop));
         pop = sort_population_by_cost(pop);
+
+        // Report only improvements
         if pop[0].0 < last_cost {
             last_cost = pop[0].0;
             let prog = pop[0].1.iter().collect::<String>();
             println!("Generation {:5} Cost {:5}: {} \t-> {:?}", tries, last_cost, 
                 prog,
-                sbrain::fixed_evaluate(&prog, Some(vec![1,2,3,4,5]), Some(100)).output);
+                sbrain::fixed_evaluate(&prog, Some(vec![0]), Some(MAX_RUNTIME)).output);
         }
+
+        // If the cost is zero, this generation has won!
         if pop[0].0 == 0 { break; }
     }
 
     let p = pop.into_iter().nth(0).unwrap();
-    let res = sbrain::fixed_evaluate(&(p.1.iter().collect::<String>()), Some(vec![1,2,3,4,5]), Some(100));
+    let res = sbrain::fixed_evaluate(&(p.1.iter().collect::<String>()), Some(vec![0]), Some(MAX_RUNTIME));
     println!("Program found after {} tries.", tries);
     println!("{}", p.1.iter().collect::<String>());
     println!("Ran for {} cycles and {} halt.\nGave: {:?}", 

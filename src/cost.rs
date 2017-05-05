f3643c579d4ff9db1224666f59011377e879dee0
SilverWingedSeraph <lfstindall@gmail.com>
2017-05-05 11:57:32 -0500

    Refactor cost_single_target for readability

--- src/cost.rs
+++ src/cost.rs
@@ -1,8 +1,10 @@
 use sbrain;
 use rayon::prelude::*;
-use super::{Program, ProgramWithCost, Population, UncostedPopulation, Configuration};
+use super::{Program, ProgramWithCost, Population, UncostedPopulation, Configuration, Cost};
+use std::cmp::{min, max};
 
-const LENGTH_WEIGHT: u64 = 1024;
+const LENGTH_WEIGHT: Cost = 1024;
+const UNIT_COST: Cost = 1;
 
 fn cost_program(program: &Program, cfg: &Configuration) -> u64 {
     let mut cost = 0;
@@ -13,19 +15,25 @@ fn cost_program(program: &Program, cfg: &Configuration) -> u64 {
 }
 
 fn cost_single_target(program: &Program, input: &[u32], target: &[u32]) -> u64 {
-    let res = sbrain::fixed_evaluate(&(program.iter().collect::<String>()), Some(Vec::from(input)), Some(100));
+    // Evaluate the program
+    let program_source = program.iter().collect::<String>();
+    let res = sbrain::fixed_evaluate(&program_source, Some(Vec::from(input)), Some(100));
+
+    let mut cost;
 
     // Score for length
-    let mut score = 
-        if target.len() > res.output.len() {target.len() - res.output.len()}
-        else {res.output.len() - target.len()} as u64 * LENGTH_WEIGHT;
+    let max_length = max(target.len(), res.output.len()) as Cost;
+    let min_length = min(target.len(), res.output.len()) as Cost;
+    cost = (max_length - min_length) * LENGTH_WEIGHT * UNIT_COST;
 
-    for (expected, actual) in res.output.into_iter().zip(target.into_iter()) {
-        if expected != *actual as u32 {
-            score += i64::abs(expected as i64 - *actual as i64) as u64;
-        }
+    // Score for value
+    for (a, b) in res.output.into_iter().zip(target.into_iter()) {
+        let max_value = max(a, *b) as Cost;
+        let min_value = min(a, *b) as Cost;
+        cost += max_value - min_value * UNIT_COST;
     }
-    score
+
+    cost
 }
 
 pub fn cost_population(uncosted_population: UncostedPopulation, cfg: &Configuration) -> Population {
